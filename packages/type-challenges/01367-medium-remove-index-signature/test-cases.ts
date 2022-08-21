21116d568506b7b7f7260f9e9213199a60174d54
xlearns <2447684095@qq.com>
2022-08-21 22:23:27 +0800

    feat: add 1367 for type-challenges

---
+++ packages/type-challenges/01367-medium-remove-index-signature/test-cases.ts
@@ -0,0 +1,31 @@
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Foo = {
+    [key: string]: any
+    foo(): void
+  }
+  
+  type Bar = {
+    [key: number]: any
+    bar(): void
+    0: string
+  }
+  
+  const foobar = Symbol('foobar')
+  type FooBar = {
+    [key: symbol]: any
+    [foobar](): void
+  }
+  
+  type Baz = {
+    bar(): void
+    baz: string
+  }
+  
+  type test = RemoveIndexSignature<FooBar>
+  type cases = [
+    Expect<Equal<RemoveIndexSignature<Foo>, { foo(): void }>>,
+    Expect<Equal<RemoveIndexSignature<Bar>, { bar(): void; 0: string }>>,
+    Expect<Equal<RemoveIndexSignature<FooBar>, { [foobar](): void }>>,
+    Expect<Equal<RemoveIndexSignature<Baz>, { bar(): void; baz: string }>>,
+  ]
